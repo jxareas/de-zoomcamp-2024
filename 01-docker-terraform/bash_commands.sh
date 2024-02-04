41fb143761859d74ba19cf74ab8f8d8d35aa2be8
Jon Areas <areasjx@gmail.com>
2024-02-04 00:41:19 -0600

    Dockerized `ingest_data` py script

--- 01-docker-terraform/bash_commands.sh
+++ 01-docker-terraform/bash_commands.sh
@@ -13,3 +13,11 @@ pgcli -h localhost -p 5431 -u root -d ny_taxi
 docker run -it
 -e PGADMIN_DEFAULT_EMAIL="admin@admin.com"
 -e PGADMIN_DEFAULT_PASSWORD="root" -p 8080:80 dpage/pgadmin4
+
+# Executing the ingest data python script
+python ingest_data.py --user=root --password=root --host=localhost --port=5431 --database=ny_taxi --table=yellow_taxi_data --url=https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz
+
+# Creating a docker container with from the Ingest Data Docker image
+docker run -it --network pg-network taxi_ingest:v001 --user=root --password=root --host=postgres13 --port=5431 --database=ny_taxi --table=yellow_taxi_data --url=https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz
+
+
